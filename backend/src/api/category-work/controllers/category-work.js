26ee4559c47ae31ff0872926bc4b8c9963212839
Burak Kuyucaklı <burak@Buraks-MacBook-Pro-2.local>
2022-05-01 18:18:14 +0300

    Frontend framework is next.js

---
+++ backend/src/api/category-work/controllers/category-work.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  category-work controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::category-work.category-work');
---
+++ backend/src/api/category-work/services/category-work.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-work service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::category-work.category-work');
