685aed3bc249330be550d66f409017d944784a68
Burak Kuyucaklı <burak.kuyucakli@gmail.com>
2022-06-01 19:45:08 +0300

    Remove framer-motion Framer-motion becomes buggy in built production project

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,7 +1,6 @@
 import Nav from '../components/Nav'
 import Logo from './Logo'
 import SeoHead from '../components/SeoHead'
-import { LazyMotion, domAnimation, m, AnimatePresence } from "framer-motion"
 import { Layout } from '../types'
 
 
@@ -25,7 +24,7 @@ function Layout({
     const breadCrumbs = ['', 'ui-ux', 'çizim', 'blog']
 
     return (
-        <LazyMotion features={domAnimation}>
+        <>
 
             <SeoHead {...seoData} />
 
@@ -39,27 +38,18 @@ function Layout({
 
             </header>
 
-            <AnimatePresence
-                exitBeforeEnter
-                initial={false}
-                onExitComplete={() => window.scrollTo(0, 0)}
-            >
-                <m.main
+           
+                <main
                     key={`p${navIndex}`}
                     className={mainClass}
                     style={mainStyle}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ delay: 0.2 }}
                 >
                     {children}
-                </m.main>
-            </AnimatePresence>
-
+                </main>
+           
             <footer id="page-footer"></footer>
 
-        </LazyMotion>
+        </>
     )
 }
 
