25cb94d577e269f84a1474a19d8d79a4c82715ea
Burak Kuyucaklı <burak.kuyucakli@gmail.com>
2022-06-02 14:20:37 +0300

    Write your custom viewport meta tag

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,15 +1,21 @@
 import '../styles/reset.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import Layout from '../components/Layout'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
+
   return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+      </Head>
       <Layout {...pageProps} >
         <Component {...pageProps} />
       </Layout>
+    </>
   )
 }
 
